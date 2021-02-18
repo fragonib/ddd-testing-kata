22e54bec7d623b9905e99a22fc372b7a78f8096c
Francisco González <fragonib@gmail.com>
2021-02-18 15:29:53 +0100

    :recycle: Clean up

--- weather/src/main/kotlin/clean/the/forest/weather/infraestructure/config/WeatherConfig.kt
+++ weather/src/main/kotlin/clean/the/forest/weather/infraestructure/config/WeatherConfig.kt
@@ -1,10 +1,7 @@
 package clean.the.forest.weather.infraestructure.config
 
 import clean.the.forest.weather.application.WeatherOfParticularAreaUseCase
-import clean.the.forest.weather.infraestructure.AreaRepository
-import clean.the.forest.weather.infraestructure.InMemoryAreaRepository
-import clean.the.forest.weather.infraestructure.OpenWeatherProvider
-import clean.the.forest.weather.infraestructure.WeatherProvider
+import clean.the.forest.weather.infraestructure.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.beans.factory.annotation.Value
