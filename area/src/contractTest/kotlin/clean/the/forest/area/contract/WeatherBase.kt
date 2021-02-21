991a5bdf645ed70270c5ebee827cbec8e25661b8
Francisco González Ibáñez <fragonib@gmail.com>
2021-02-22 00:31:19 +0100

    :recycle: Use JUNIT5 when possible

--- area/src/contractTest/kotlin/clean/the/forest/area/contract/WeatherBase.kt
+++ area/src/contractTest/kotlin/clean/the/forest/area/contract/WeatherBase.kt
@@ -2,26 +2,23 @@ package clean.the.forest.area.contract
 
 import clean.the.forest.area.application.ReportCheckabilityOfParticularAreaUseCase
 import clean.the.forest.area.application.ReportWeatherOfAllKnownAreasUseCase
-import clean.the.forest.area.infrastructure.AreaController
-import clean.the.forest.area.infrastructure.ConflictWithExistingArea
-import clean.the.forest.area.infrastructure.WeatherConfig
 import clean.the.forest.area.infrastructure.WeatherController
 import clean.the.forest.area.model.Area
 import clean.the.forest.area.model.WeatherReport
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import reactor.core.publisher.Flux
 import java.time.LocalDateTime
 
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [WeatherController::class],
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = ["server.port=0"])
