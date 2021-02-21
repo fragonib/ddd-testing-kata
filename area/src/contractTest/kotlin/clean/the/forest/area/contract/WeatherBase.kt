fb74a089a59b1a758d70b8642b0b33e14bd9fb2c
Francisco González Ibáñez <fragonib@gmail.com>
2021-02-21 23:56:11 +0100

    :recycle: Improve contract tests

--- area/src/contractTest/kotlin/clean/the/forest/area/contract/WeatherBase.kt
+++ area/src/contractTest/kotlin/clean/the/forest/area/contract/WeatherBase.kt
@@ -1,26 +1,60 @@
 package clean.the.forest.area.contract
 
+import clean.the.forest.area.application.ReportCheckabilityOfParticularAreaUseCase
+import clean.the.forest.area.application.ReportWeatherOfAllKnownAreasUseCase
+import clean.the.forest.area.infrastructure.AreaController
+import clean.the.forest.area.infrastructure.ConflictWithExistingArea
 import clean.the.forest.area.infrastructure.WeatherConfig
 import clean.the.forest.area.infrastructure.WeatherController
+import clean.the.forest.area.model.Area
+import clean.the.forest.area.model.WeatherReport
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
+import org.junit.runner.RunWith
+import org.mockito.Mockito
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.junit4.SpringRunner
+import reactor.core.publisher.Flux
+import java.time.LocalDateTime
 
 
-@SpringBootTest(classes = [WeatherConfig::class, WeatherController::class],
+@RunWith(SpringRunner::class)
+@SpringBootTest(classes = [WeatherController::class],
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = ["server.port=0"])
 @EnableAutoConfiguration
 abstract class WeatherBase {
+
+    @MockBean
+    private lateinit var reportAllKnownAreasUseCase: ReportWeatherOfAllKnownAreasUseCase
+
+    @MockBean
+    private lateinit var reportCheckabilityOfParticularAreaUseCase: ReportCheckabilityOfParticularAreaUseCase
 
     @LocalServerPort
     var port = 0
 
     @BeforeEach
     fun setup() {
-        RestAssured.baseURI = "http://localhost:" + port
+        RestAssured.baseURI = "http://localhost:$port"
+
+        Mockito.`when`(reportAllKnownAreasUseCase.report())
+            .thenReturn(Flux.just(
+                WeatherReport(
+                    area = Area("One area", 43.05, -2.57, "ES"),
+                    weatherCondition = "Clouds",
+                    date = LocalDateTime.now()
+                ),
+                WeatherReport(
+                    area = Area("Other area", 42.97, -2.29, "ES"),
+                    weatherCondition = "Clear",
+                    date = LocalDateTime.now()
+                )
+
+            ))
     }
 
 }
