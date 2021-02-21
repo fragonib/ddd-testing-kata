08f63c4f2234986f094e3b00e9b25ce3f8d2b12d
Francisco González Ibáñez <fragonib@gmail.com>
2021-02-21 16:15:19 +0100

    :red_circle: [STEP 3] Add non-passing unit tests

--- area/src/test/groovy/clean/the/forest/area/infrastructure/InMemoryAreaRepositorySpec.groovy
+++ area/src/test/groovy/clean/the/forest/area/infrastructure/InMemoryAreaRepositorySpec.groovy
@@ -1,38 +1,40 @@
 package clean.the.forest.area.infrastructure
 
-import clean.the.forest.shared.testing.TestClassification
+import clean.the.forest.area.model.Area
 import clean.the.forest.area.model.Country
 import clean.the.forest.area.model.GeoPos
+import clean.the.forest.shared.testing.TestClassification
 import org.junit.jupiter.api.Tag
 import spock.lang.Specification
-
 
 @Tag(TestClassification.INTEGRATION)
 class InMemoryAreaRepositorySpec extends Specification {
 
-    AreaRepository sut = new InMemoryAreaRepository()
 
-    def 'area repository contains 3 areas "Ipiñaburu, Ibarra, Zegama"'() {
+    def 'all known areas contains "Ipiñaburu, Ibarra, Zegama"'() {
+
+        given:
+        AreaRepository sut = new InMemoryAreaRepository()
 
         when:
-        def knownAreas = sut.allKnown()
-                .collectList()
-                .block()
+        def knownAreas = sut.allKnown().collectList().block()
 
         then:
         knownAreas.size() == 3
         knownAreas.name == ["Ipiñaburu", "Ibarra", "Zegama"]
         knownAreas.position.lat == [43.07d, 43.05d, 42.97d]
         knownAreas.position.lon == [-2.75d, -2.57d, -2.29d]
-        knownAreas.country.code.every {it == "ES"}
+        knownAreas.country.code.every { it == "ES" }
 
     }
 
-    def 'area "#areaName" is known'() {
+    def 'finding area "#areaName" is known'() {
+
+        given:
+        AreaRepository sut = new InMemoryAreaRepository()
 
         when:
-        def foundArea = sut.findByName(areaName)
-                .block()
+        def foundArea = sut.findByName(areaName).block()
 
         then:
         foundArea.country == new Country(countryCode)
@@ -45,11 +47,13 @@ class InMemoryAreaRepositorySpec extends Specification {
         "zegama"    | 42.97 | -2.29 | "ES"
     }
 
-    def 'area "#areaName" is NOT known'() {
+    def 'finding area "#areaName" is NOT known'() {
+
+        given:
+        AreaRepository sut = new InMemoryAreaRepository()
 
         when:
-        sut.findByName(areaName)
-                .block()
+        sut.findByName(areaName).block()
 
         then:
         IllegalArgumentException ex = thrown()
@@ -59,6 +63,36 @@ class InMemoryAreaRepositorySpec extends Specification {
         areaName  || expectedMessage
         "unknown" || "There is no area called [unknown]"
         "ipiñabu" || "There is no area called [ipiñabu]"
+    }
+
+    def 'adding an area should aggregate it to known areas'() {
+
+        given:
+        AreaRepository sut = new InMemoryAreaRepository()
+
+        when:
+        def newArea = new Area("newArea", 40, -2, "ES")
+        sut.addArea(newArea).block()
+
+        then:
+        def knownAreas = sut.allKnown().toIterable()
+        knownAreas.any { it == newArea }
+
+    }
+
+    def 'adding an yet known area should complain'() {
+
+        given:
+        AreaRepository sut = new InMemoryAreaRepository()
+
+        when:
+        def areaName = new Area("Ipiñaburu", 40, -2, "ES")
+        sut.addArea(areaName).block()
+
+        then:
+        IllegalStateException ex = thrown()
+        ex.message == "There is yet known area called [Ipiñaburu]"
+
     }
 
 }
