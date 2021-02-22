28950bd86b490837c325eaf70c5b5777fb486116
Francisco González <fragonib@gmail.com>
2021-02-22 09:02:37 +0100

    :hammer: FIX gradle issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,13 @@ pluginManagement {
     }
     resolutionStrategy {
         val bootVersion: String by settings
-        val verifierVersion: String by settings
+        val contractVerifierPluginVersion: String by settings
         eachPlugin {
             if (requested.id.id == "org.springframework.boot") {
                 useModule("org.springframework.boot:spring-boot-gradle-plugin:$bootVersion")
             }
             if (requested.id.id == "org.springframework.cloud.contract") {
-                useModule("org.springframework.cloud:spring-cloud-contract-gradle-plugin:$verifierVersion")
+                useModule("org.springframework.cloud:spring-cloud-contract-gradle-plugin:$contractVerifierPluginVersion")
             }
         }
     }
