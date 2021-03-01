feb6a1e61a2acdb0d8626e3bb86025e2a77a647a
Francisco González <fragonib@gmail.com>
2021-03-01 19:50:22 +0100

    :recycle: Repackaging

--- area/src/test/kotlin/clean/the/forest/area/infrastructure/package-info.kt
+++ area/src/test/kotlin/clean/the/forest/area/infrastructure/package-info.kt
@@ -1,6 +1,6 @@
 /**
  * ControllerTest hosted here are overlapping with Contract tests. However Contract tests
  * written in code (Grooovy, Kotlin) are not fully working right now, so meanwhile we keep
- * theses tests to be more expressive.
+ * theses tests in order to be more expressive.
  */
 package clean.the.forest.area.infrastructure
