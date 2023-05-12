7a48fc13a9cacc0dd6368369505a350368b577fd
Francisco González <fragonib@gmail.com>
2023-05-12 17:57:22 +0200

    refactor: extract common Spec for working with collaborators

--- area/src/test/groovy/clean/the/forest/area/infrastructure/CollaboratorSpec.groovy
+++ area/src/test/groovy/clean/the/forest/area/infrastructure/CollaboratorSpec.groovy
@@ -2,19 +2,37 @@ package clean.the.forest.area.infrastructure
 
 import clean.the.forest.shared.testing.integration.CollaboratorLifecycle
 import clean.the.forest.shared.testing.integration.CollaboratorsConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.bind.Binder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
+@ContextConfiguration(classes = [TestConfig])
 class CollaboratorSpec extends Specification {
 
     CollaboratorLifecycle collaboratorLifecycle
 
-    def setupO(CollaboratorsConfig collaboratorsConfig) {
+    def setup() {
         collaboratorLifecycle = new CollaboratorLifecycle(collaboratorsConfig)
         collaboratorLifecycle.setupCollaborators()
     }
 
     def cleanup() {
-        collaboratorLifecycle.teardownCollaborators()
+        collaboratorLifecycle?.teardownCollaborators()
+    }
+
+    @Autowired
+    private CollaboratorsConfig collaboratorsConfig
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        CollaboratorsConfig collaboratorsConfig(Environment env) {
+            Binder.get(env).bindOrCreate('collaborators-config', CollaboratorsConfig)
+        }
     }
 
 }
--- area/src/test/groovy/clean/the/forest/area/infrastructure/OpenWeatherProviderIT.groovy
+++ area/src/test/groovy/clean/the/forest/area/infrastructure/OpenWeatherProviderIT.groovy
@@ -14,21 +14,16 @@ import org.springframework.test.context.ContextConfiguration
 import spock.lang.Subject
 
 @SpringBootTest
-@ContextConfiguration(classes = [TestConfig])
 @ActiveProfiles("isolated")
 class OpenWeatherProviderIT extends CollaboratorSpec {
-
-    @Autowired
-    CollaboratorsConfig collaboratorsConfig
 
     @Subject
     WeatherProvider sut
 
     def setup() {
-        super.setupO(collaboratorsConfig)
-        def openWeatherMock = collaboratorLifecycle.collaboratorMock(Collaborator.OPEN_WEATHER.literal)
-        def open_weather_api_key = 'dummy_api_key'
-        sut = new OpenWeatherProvider(openWeatherMock.baseUrl(), open_weather_api_key)
+        def openWeatherMockBaseUrl = collaboratorLifecycle.collaboratorMock(Collaborator.OPEN_WEATHER.literal).baseUrl()
+        def openWeatherApiKey = 'dummy_api_key'
+        sut = new OpenWeatherProvider(openWeatherMockBaseUrl, openWeatherApiKey)
     }
 
     def "weather of #name by geo position"() {
@@ -44,14 +39,6 @@ class OpenWeatherProviderIT extends CollaboratorSpec {
         "Calderona"   | 39.67d | -0.43d || "Clear"
         "Mariola"     | 38.72d | -0.53d || "Clouds"
         "Penyagolosa" | 40.23d | -0.29d || "Clear"
-    }
-
-    @Configuration
-    static class TestConfig {
-        @Bean
-        CollaboratorsConfig collaboratorsConfig(Environment env) {
-            Binder.get(env).bindOrCreate('collaborators-config', CollaboratorsConfig)
-        }
     }
 
 }
