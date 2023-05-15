c98cd6336de6cba4d47bbe7a26a7511e2f93304f
Francisco González <fragonib@gmail.com>
2023-05-15 19:33:42 +0200

    refactor: improve readability of SUT

--- area/src/test/groovy/clean/the/forest/area/application/ReportCheckabilityOfParticularAreaUseCaseTest.groovy
+++ area/src/test/groovy/clean/the/forest/area/application/ReportCheckabilityOfParticularAreaUseCaseTest.groovy
@@ -8,26 +8,29 @@ import clean.the.forest.shared.testing.TestClassification
 import org.junit.jupiter.api.Tag
 import reactor.core.publisher.Mono
 import spock.lang.Specification
+import spock.lang.Subject
 import spock.lang.Unroll
 
 
-@Tag(TestClassification.UNIT)
 class ReportCheckabilityOfParticularAreaUseCaseTest extends Specification {
+
+    AreaRepository areaRepository = Mock()
+    WeatherProvider weatherProvider = Mock()
+
+    @Subject
+    def sut = new ReportCheckabilityOfParticularAreaUseCase(areaRepository, weatherProvider)
 
     @Unroll
     def 'checkability status of area "#areaName" should be "#expectedCheckability"'() {
 
         given: "External dependencies stubbed"
-        AreaRepository areaRepository = Stub()
         areaRepository.findByName(areaName) >> Mono.just(
                 new Area(areaName, expectedLat, expectedLon, expectedCountry)
         )
 
-        WeatherProvider weatherProvider = Stub()
         weatherProvider.byGeoPosition(new GeoPos(expectedLat, expectedLon)) >> Mono.just(expectedWeather)
 
         when: "request weather by area name"
-        def sut = new ReportCheckabilityOfParticularAreaUseCase(areaRepository, weatherProvider)
         def weatherReport = sut.report(areaName).block()
 
         then: "repost should has expected area and weather condition"
--- area/src/test/groovy/clean/the/forest/area/application/ReportWeatherOfAllKnownAreasUseCaseTest.groovy
+++ area/src/test/groovy/clean/the/forest/area/application/ReportWeatherOfAllKnownAreasUseCaseTest.groovy
@@ -9,30 +9,33 @@ import org.junit.jupiter.api.Tag
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import spock.lang.Specification
+import spock.lang.Subject
 import spock.lang.Unroll
 
 
-@Tag(TestClassification.UNIT)
 class ReportWeatherOfAllKnownAreasUseCaseTest extends Specification {
+
+    AreaRepository areaRepository = Mock()
+    WeatherProvider weatherProvider = Mock()
+
+    @Subject
+    def sut = new ReportWeatherOfAllKnownAreasUseCase(areaRepository, weatherProvider)
 
     @Unroll
     def 'weather of area "#areaName" should be "#expectedWeather"'() {
 
         given: "External dependencies stubbed"
-        AreaRepository areaRepository = Stub()
         areaRepository.allKnown() >> Flux.just(
                 new Area("Calderona", 39.67, -0.43, "ES"),
                 new Area("Mariola", 38.72, -0.53, "ES"),
                 new Area("Penyagolosa", 40.23, -0.29, "ES")
         )
 
-        WeatherProvider weatherProvider = Stub()
         weatherProvider.byGeoPosition(new GeoPos(39.67, -0.43)) >> Mono.just("Clouds")
         weatherProvider.byGeoPosition(new GeoPos(38.72, -0.53)) >> Mono.just("Clear")
         weatherProvider.byGeoPosition(new GeoPos(40.23, -0.29)) >> Mono.just("Drizzle")
 
         when: "request weather of all known areas"
-        def sut = new ReportWeatherOfAllKnownAreasUseCase(areaRepository, weatherProvider)
         def checkoutReports = sut.report().toIterable()
 
         then: "report should has expected area and weather condition"
