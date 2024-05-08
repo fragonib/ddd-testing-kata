cd55a456fa345a7beba14507776f2f1f51b89a2e
Francisco González Ibáñez <fragonib@gmail.com>
2024-05-08 17:12:16 +0200

    docs: clean up typos in documentation

--- area/src/test/groovy/clean/the/forest/area/infrastructure/package-info.kt
+++
@@ -1,6 +0,0 @@
-/**
- * ControllerTest hosted here are overlapping with Contract tests. However Contract tests
- * written in code (Grooovy, Kotlin) are not fully working right now, so meanwhile we keep
- * these tests in order to be more expressive.
- */
-package clean.the.forest.area.infrastructure
